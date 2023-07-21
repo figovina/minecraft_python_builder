fbd8b1d8ce3f2d4b7c0b587e3fe82f08d34e422d
figovina <uarukilen2001@gmail.com>
2023-07-21 22:08:28 +0300

    v1.2
    added blacklist, changed structure of templates list

--- builder.py
+++ builder.py
@@ -14,7 +14,21 @@ y_size = 3
 
 x_min = 0
 z_min = 0
-y_min = 10
+y_min = 0
+
+building_folder_name = "test"
+
+enable_blacklist = True
+
+block_black_list = [
+    (255, 255, 255)
+]
+
+picture_list = [
+    "1.bmp",
+    "2.bmp",
+    "3.bmp"
+]
 
 block_list = {
     (255, 255, 255) : 0,  # air
@@ -25,14 +39,7 @@ block_list = {
     (0, 255, 255): 98,    # stone bricks
     (255, 0, 255): 80,    # snow block
     (0, 0, 0): 1          # stone
-
 }
-
-picture_list = [
-    "1.bmp",
-    "2.bmp",
-    "3.bmp"
-]
 
 #-------------------------------------------------------------------------
 #setup
@@ -50,6 +57,10 @@ z_pos = 0
 block = ""
 rgb = ""
 
+verify_number = 0
+black_list_length = len(block_black_list)
+verify_result = 0
+
 time_start = 0
 time_stop = 0
 time_all = 0
@@ -61,7 +72,7 @@ time_start = time.time()
 
 for y in range(0, y_size):
 
-    with Image.open("E:/minecraft-spigot_server/building/%s" % picture_list[y]) as im:
+    with Image.open("E:/minecraft-spigot_server/buildings/%s/%s" % (building_folder_name, picture_list[y])) as im:
         #im.show()
         px = im.load()
 
@@ -78,10 +89,24 @@ for y in range(0, y_size):
             rgb = px[x, z]
             block = block_list[rgb]
 
-            mc.setBlock(x_pos, y_pos, z_pos, block)
-            #print("set %s at X = %s, Y = %s, Z = %s" % (block, x_pos, y_pos, z_pos))
+            if enable_blacklist:
+                for verify_number in range(0, black_list_length):
+                    if rgb == block_black_list[verify_number]:
+                        pass
+                    else:
+                        verify_result = verify_result + 1
 
-            time.sleep(0.01)
+                if verify_result == black_list_length:
+                    mc.setBlock(x_pos, y_pos, z_pos, block)
+                    verify_result = 0
+                    time.sleep(0.01)
+                else:
+                    verify_result = 0
+            else:
+                mc.setBlock(x_pos, y_pos, z_pos, block)
+                time.sleep(0.01)
+
+            #print("set %s at X = %s, Y = %s, Z = %s" % (block, x_pos, y_pos, z_pos))
 
 time_stop = time.time()
 time_all = time_stop - time_start
