dccf573848a0865cae0dee9ec9a4af0fde716b39
figovina <uarukilen2001@gmail.com>
2023-08-18 11:57:18 +0300

    tests

---
+++ tests/test2.py
@@ -0,0 +1,2 @@
+x_list = [5]
+print(x_list[(len(x_list) - 1)])
--- tests/test3.py
+++ tests/test3.py
@@ -1,6 +1,3 @@
-
 from PIL import Image
 with Image.open("buildings/test/1.bmp") as im:
     im.show()
-    px = im.load()
-print(px[0, 0])
